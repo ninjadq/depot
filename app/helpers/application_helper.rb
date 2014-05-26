e4f693b25a8264ff02e767280104af52b0b39faf
Qian Deng <dengqian0826@gmail.com>
2014-05-26 17:56:58 +0800

    Ajax cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def hidden_div_if(conditon, attributes = {}, &block)
+		if conditon
+			attributes["style"] = "display: none"
+		end
+		content_tag("div", attributes, &block)
+	end
 end
